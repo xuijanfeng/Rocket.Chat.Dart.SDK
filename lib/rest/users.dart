b0ef514a3a2b1d494e8de2e85968f33fbe126e16
xujianfeng <xujianfeng@infohold.com.cn>
2019-11-22 13:59:48 +0800

    根据用户名或者用户id查询账户登陆状态，什么都传，那么查询自己的

--- lib/rest/users.dart
+++ lib/rest/users.dart
@@ -26,8 +26,7 @@ abstract class _ClientUsersMixin implements _ClientWrapper {
   }
 
   // savePushToken stores a push token and returns its id
-  Future<String> savePushToken(
-      String id, String token, String type, String appName) {
+  Future<String> savePushToken(String id, String token, String type, String appName) {
     Completer<String> completer = Completer();
     http
         .post('${_getUrl()}/push.token',
@@ -45,6 +44,31 @@ abstract class _ClientUsersMixin implements _ClientWrapper {
         .then((response) {
       _hackResponseHeader(response);
       final data = json.decode(response.body)['result']['_id'];
+      completer.complete(data);
+    }).catchError((error) => completer.completeError(error));
+    return completer.future;
+  }
+
+//根据用户名或者用户id查询账户登陆状态，什么都传，那么查询自己的
+  Future<String> getPresence(String userId, String username) {
+    Completer<String> completer = Completer();
+    String body = null;
+    if (userId != null) {
+      body = json.encode(<String, String>{'userId': userId});
+    } else if (username != null) {
+      body = json.encode(<String, String>{'username': username});
+    }
+    http
+        .post('${_getUrl()}/users.getPresence',
+            headers: {
+              'Content-Type': 'application/json',
+              'X-User-Id': _auth._id,
+              'X-Auth-Token': _auth._token,
+            },
+            body: body)
+        .then((response) {
+      _hackResponseHeader(response);
+      final data = json.decode(response.body)['presence'];
       completer.complete(data);
     }).catchError((error) => completer.completeError(error));
     return completer.future;
